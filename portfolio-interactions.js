9e27751cdf223fadcf775a188fbc167ee5f196cc
Connor <154204465+connoreveretts@users.noreply.github.com>
2024-02-11 22:53:01 -0800

    Add files via upload

--- portfolio-interactions.js
+++ portfolio-interactions.js
@@ -1,7 +1,23 @@
 document.querySelectorAll('.job-header').forEach(header => {
     header.addEventListener('click', () => {
-        // This will find the next sibling element with the class 'job-description' and toggle its display
         const description = header.nextElementSibling;
         description.style.display = description.style.display === 'block' ? 'none' : 'block';
     });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const textElement = document.getElementById('dynamicTyping');
+    const textToType = textElement.innerText;
+    textElement.innerText = ''; 
+
+    let index = 0;
+    function typeLetter() {
+        if (index < textToType.length) {
+            textElement.innerText += textToType.charAt(index);
+            index++;
+            setTimeout(typeLetter, 25);
+        }
+    }
+
+    typeLetter(); 
 });
