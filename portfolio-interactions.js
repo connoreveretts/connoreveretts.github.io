32f747cb9159d2276e9585c5b0b290bad7279efe
Connor <154204465+connoreveretts@users.noreply.github.com>
2024-03-28 17:28:20 -0700

    Add files via upload

--- portfolio-interactions.js
+++ portfolio-interactions.js
@@ -111,3 +111,34 @@ document.addEventListener('DOMContentLoaded', function() {
         window.scrollTo({top: 0, behavior: 'smooth'});
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var form = document.getElementById("myForm");
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      var data = new FormData(form);
+      fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: {
+          'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          document.getElementById("myForm").style.display = 'none'; // Optional: Hide form
+          document.getElementById("thankYouMessage").style.display = 'block';
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              alert(data["errors"].map(error => error["message"]).join(", "));
+            } else {
+              alert("Oops! There was a problem submitting your form");
+            }
+          });
+        }
+      }).catch(error => {
+        alert("Oops! There was a problem submitting your form");
+      });
+    });
+  });
+  
